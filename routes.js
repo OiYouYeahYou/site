0f0ddacedcec5b2d9b99c7e2dbb7dd981de9ad69
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-01-07 23:35:48 +0000

    Setting up feed

--- routes.js
+++ routes.js
@@ -1,12 +1,29 @@
 const router = module.exports = require( 'express' ).Router();
 
-var homeOptions = {
-	title: 'Express',
+const exampleFeed = [ {
+		heading: 'Totally not a blog post!',
+		content: 'Do ... I put words here?',
+		time: ( new Date( Date.now() ) ).toISOString(),
+	},
+	{
+		content: 'Hello World!',
+		time: ( new Date( Date.now() ) ).toISOString(),
+	},
+	{
+		content: 'In the begining!',
+		place: 'Twooter',
+		time: new Date( 0 ).toISOString(),
+	},
+];
+
+const homeOptions = {};
+const feedOptions = {
+	feed: exampleFeed
 };
 
 simpleRouter( '/about', 'about', homeOptions );
 simpleRouter( '/portfolio', 'portfolio', homeOptions );
-simpleRouter( '/feed', 'feed', homeOptions );
+simpleRouter( '/feed', 'feed', feedOptions );
 simpleRouter( '/contact', 'contact', homeOptions );
 simpleRouter( '/', 'index', homeOptions );
 
