97a5e266cc6ce10175af6dacbea3e77e25c99fda
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-01-08 22:44:41 +0000

    Removing excess

--- app.js
+++ app.js
@@ -8,18 +8,19 @@ const exphbs = require( 'express-handlebars' );
 
 const routes = require( './routes' );
 
-const app = module.exports = express();
+const pathView = path.join( __dirname, 'views' );
+const pathPublic = path.join( __dirname, 'public' );
 
-const env = process.env.NODE_ENV || 'development';
-app.locals.ENV = env;
-app.locals.ENV_DEVELOPMENT = env == 'development';
+const app = module.exports = express();
+app.locals.ENV = process.env.NODE_ENV || 'development';
+app.locals.ENV_DEVELOPMENT = app.locals.ENV == 'development';
 
 // view engine setup
 app.engine( 'handlebars', exphbs( {
 	defaultLayout: 'main',
 	partialsDir: [ 'views/partials/' ]
 } ) );
-app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'views', pathView );
 app.set( 'view engine', 'handlebars' );
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
@@ -27,6 +28,6 @@ app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( cookieParser() );
-app.use( express.static( path.join( __dirname, 'public' ) ) );
+app.use( express.static( pathPublic ) );
 
 app.use( '/', routes );
