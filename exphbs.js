51d5db464fed3d9e1742139ea4ee97d9fc4665b0
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-01-14 21:13:02 +0000

    [min] Adding moment to make dates look nicer

--- exphbs.js
+++ exphbs.js
@@ -1,6 +1,12 @@
 const exphbs = require( 'express-handlebars' );
+const moment = require( 'moment' );
 
 module.exports =  exphbs( {
 	defaultLayout: 'main',
 	partialsDir: [ 'views/partials/' ],
+	helpers: {
+		friendlyDateTime: ( d ) => {
+			return moment( d ).format( 'h:hh - d MMM YYYY' );
+		}
+	},
 } )
