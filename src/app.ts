3383bae432771aa2d849ff0bb3322cd77fd307f3
Jason <github@jallan.co.uk>
2018-07-19 22:37:34 +0100

    formatting app.js

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
 require('source-map-support').install()
 
-import * as express from 'express';
-import { join } from 'path';
+import * as express from 'express'
+import { join } from 'path'
 // import favicon from 'serve-favicon';
-import * as cookieParser from 'cookie-parser';
-import { urlencoded, json } from 'body-parser';
+import * as cookieParser from 'cookie-parser'
+import { urlencoded, json } from 'body-parser'
 
-import exphbs from './exphbs';
-import { redirect } from './redirect';
-import { routes } from './routes';
+import exphbs from './exphbs'
+import { redirect } from './redirect'
+import { routes } from './routes'
 
-const pathView = join(__dirname, '../views');
-const pathPublic = join(__dirname, '../public');
+const pathView = join(__dirname, '../views')
+const pathPublic = join(__dirname, '../public')
 
-export const app = express();
-app.locals.ENV = process.env.NODE_ENV || 'development';
-app.locals.ENV_DEVELOPMENT = app.locals.ENV == 'development';
-app.locals.mainMenu = ['about', 'portfolio', 'feed', 'status', 'contact'];
+export const app = express()
+app.locals.ENV = process.env.NODE_ENV || 'development'
+app.locals.ENV_DEVELOPMENT = app.locals.ENV == 'development'
+app.locals.mainMenu = ['about', 'portfolio', 'feed', 'status', 'contact']
 
 // view engine setup
-app.engine('handlebars', exphbs);
-app.set('views', pathView);
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs)
+app.set('views', pathView)
+app.set('view engine', 'handlebars')
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
-[
+;[
 	[json()],
 	[urlencoded({ extended: true })],
 	[cookieParser()],
