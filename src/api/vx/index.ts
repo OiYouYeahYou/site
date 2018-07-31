6ff10b3f3073c167eb06f596a91889059ba01090
Jason <github@jallan.co.uk>
2018-07-31 23:58:13 +0100

    Adding api/vx/status/:path/update/:state

--- src/api/vx/index.ts
+++ src/api/vx/index.ts
@@ -43,6 +43,24 @@ const statuses = {
 	},
 }
 
+router.use('/status/:path/update/:state', (req, res) => {
+	const { path, state } = req.params
+
+	if (statuses.hasOwnProperty(path)) {
+		const status = statuses[path]
+
+		if (!status.states.hasOwnProperty(state)) {
+			res.status(404).json({ error: `invalid: ${state}` })
+		} else {
+			status.currentState = state
+
+			res.status(200).json(status)
+		}
+	} else {
+		res.status(404).json({ error: `path not found: ${path}` })
+	}
+})
+
 router.use('/status/:path', (req, res) => {
 	const { path } = req.params
 
